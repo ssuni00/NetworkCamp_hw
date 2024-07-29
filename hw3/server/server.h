4d1a974cdf0864f09485564ef98699e6e5c5e860
ssuni00 <126534280+ssuni00@users.noreply.github.com>
2024-07-29 16:53:39 +0900

    Create server.h

---
+++ hw3/server/server.h
@@ -0,0 +1,34 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 1024
+
+typedef enum
+{
+    cd,
+    ls,
+    dl,
+    up
+} CommandType;
+
+typedef struct
+{
+    CommandType command;
+    char path[BUFFER_SIZE];
+} Command;
+
+#endif
