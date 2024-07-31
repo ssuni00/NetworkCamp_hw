831d19aaaeae22995b1dba3fe348c3f09234f20b
ssuni00 <126534280+ssuni00@users.noreply.github.com>
2024-07-31 19:11:46 +0900

    Update client.h

--- hw4/client/client.h
+++ hw4/client/client.h
@@ -1,1 +1,16 @@
+#ifndef CLIENT_H
+#define CLIENT_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <ncurses.h>
+#include <ctype.h>
+
+void connect_to_server(const char *address, int port);
+
+#endif
