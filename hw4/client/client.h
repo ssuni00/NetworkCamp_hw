a465c19ffd0e5a01e75c2dab7efd3c93fa3c3c7f
ssuni00 <126534280+ssuni00@users.noreply.github.com>
2024-07-31 19:47:17 +0900

    Update client.h

--- hw4/client/client.h
+++ hw4/client/client.h
@@ -11,7 +11,6 @@
 #include <ncurses.h>
 #include <ctype.h>
 
-void connect_to_server(const char *address, int port);
 void error_handling(char *message);
 char *strcasestr(const char *haystack, const char *needle);
 
